a16a9734931384f3b3f3dce941446b01e3f4d7d4
何仲奇 <hezhongqi@shulie.io>
2021-09-29 16:37:15 +0800

    swagger修改

--- takin-cloud-app/src/main/java/io/shulie/takin/app/conf/SwaggerConfig.java
+++ takin-cloud-app/src/main/java/io/shulie/takin/app/conf/SwaggerConfig.java
@@ -14,13 +14,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * The type Swagger config.
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @EnableKnife4j
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
