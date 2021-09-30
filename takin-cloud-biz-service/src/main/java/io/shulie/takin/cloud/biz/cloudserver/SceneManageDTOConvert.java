008602c71811dcd9dcff5c761743827255a68f4c
caijianying <caijianying@shulie.io>
2021-09-30 16:23:00 +0800

    编译问题

--- takin-cloud-biz-service/src/main/java/io/shulie/takin/cloud/biz/cloudserver/SceneManageDTOConvert.java
+++ takin-cloud-biz-service/src/main/java/io/shulie/takin/cloud/biz/cloudserver/SceneManageDTOConvert.java
@@ -34,7 +34,7 @@ public interface SceneManageDTOConvert {
 
     @Mappings({
         @Mapping(source = "id", target = "id"),
-        @Mapping(source = "customerId", target = "customerId"),
+        @Mapping(source = "customerId", target = "tenantId"),
         @Mapping(source = "sceneName", target = "sceneName"),
         @Mapping(source = "lastPtTime", target = "lastPtTime", dateFormat = "yyyy-MM-dd HH:mm:ss"),
         @Mapping(source = "status", target = "status")
