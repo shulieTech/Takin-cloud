364ebab559ef082cc175a21df626940ec61c0b5e
张天赐 <472546172@qq.com>
2021-10-05 09:40:38 +0800

    移除废弃项
    io.shulie.takin.cloud.ext.content.user.CloudUserCommonRequestExt

--- takin-cloud-ext/src/main/java/io/shulie/takin/cloud/ext/content/asset/AssetBalanceExt.java
+++ takin-cloud-ext/src/main/java/io/shulie/takin/cloud/ext/content/asset/AssetBalanceExt.java
@@ -1,18 +1,18 @@
 package io.shulie.takin.cloud.ext.content.asset;
 
-import java.io.Serializable;
-
-import io.shulie.takin.cloud.ext.content.user.CloudUserCommonRequestExt;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import io.shulie.takin.cloud.ext.content.user.CloudUserExt;
 
 /**
  * @author caijianying
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class AssetBalanceExt extends CloudUserCommonRequestExt implements Serializable {
+public class AssetBalanceExt extends CloudUserExt {
     @ApiModelProperty("cloud场景ID")
     private Long sceneId;
     @ApiModelProperty("cloud压测报告ID")
