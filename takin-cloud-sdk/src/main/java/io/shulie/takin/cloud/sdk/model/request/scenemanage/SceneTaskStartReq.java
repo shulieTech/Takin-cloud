a4bf484da8e54d9373424938d3a1aeab1e76dd11
zhaoyong <zhaoyong@shulie.io>
2022-02-09 14:45:56 +0800

    启动任务插件携带版本

--- takin-cloud-sdk/src/main/java/io/shulie/takin/cloud/sdk/model/request/scenemanage/SceneTaskStartReq.java
+++ takin-cloud-sdk/src/main/java/io/shulie/takin/cloud/sdk/model/request/scenemanage/SceneTaskStartReq.java
@@ -3,6 +3,8 @@ package io.shulie.takin.cloud.sdk.model.request.scenemanage;
 import java.util.List;
 
 import io.shulie.takin.cloud.ext.content.trace.ContextExt;
+import io.shulie.takin.cloud.sdk.model.request.engine.EnginePluginsRefOpen;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -22,6 +24,8 @@ public class SceneTaskStartReq extends ContextExt {
     private String resourceName;
 
     private List<Long> enginePluginIds;
+
+    private List<EnginePluginsRefOpen> enginePlugins;
 
     private Boolean leakSqlEnable;
 
