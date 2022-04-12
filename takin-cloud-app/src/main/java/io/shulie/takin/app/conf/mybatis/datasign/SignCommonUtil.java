f8ba88481c7ade87c52fd74bf3dafb58ab29f97b
pnz.zhao <zhaopnz@126.com>
2022-04-12 09:56:45 +0800

    同步优化签名插件

--- takin-cloud-app/src/main/java/io/shulie/takin/app/conf/mybatis/datasign/SignCommonUtil.java
+++ takin-cloud-app/src/main/java/io/shulie/takin/app/conf/mybatis/datasign/SignCommonUtil.java
@@ -4,6 +4,7 @@ import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.ReflectUtil;
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.shulie.takin.cloud.common.exception.TakinCloudException;
 import io.shulie.takin.cloud.common.exception.TakinCloudExceptionEnum;
 import io.shulie.takin.utils.security.MD5Utils;
@@ -63,11 +64,15 @@ public class SignCommonUtil {
             if (SqlCommandType.INSERT.equals(mappedStatement.getSqlCommandType())) {
                 //新增方法获取id
                 Field idField = ReflectUtil.getField(clz, "id");
+                if (idField == null) {
+                    //兼容 t_black_list
+                    idField = ReflectUtil.getField(clz, "BLIST_ID");
+                }
                 idField.setAccessible(true);
                 long id = Long.parseLong(idField.get(parameterObject).toString());
                 Insert insert = (Insert) CCJSqlParserUtil.parse(boundSql.getSql());
                 String tableName = insert.getTable().getName();
-                String whereStr = " where id = " + id;
+                String whereStr = " where " + idField.getAnnotation(TableId.class).value() + " = " + id;
                 String querySql = "select * from " + tableName + whereStr;
 
                 ResultSet rs = statement.getConnection().createStatement().executeQuery(querySql);
@@ -87,13 +92,8 @@ public class SignCommonUtil {
                 map.remove("update_time");
                 map.remove("UPDATE_TIME");
                 map.remove("CREATE_TIME");
-
                 String sign = MD5Utils.getInstance().getMD5(MapUtil.sort(map).toString());
-                String updateSql = "update " + tableName + "  SET sign = " + "\'" + sign + "\'" + " where id = " + id;
-
-                log.info("insert set sign info1:"+tableName+"."+id+"/"+sign);
-                log.info("insert set sign info2:"+MapUtil.sort(map).toString());
-
+                String updateSql = "update " + tableName + "  SET sign = " + "\'" + sign + "\'" + " where " + idField.getAnnotation(TableId.class).value() +" = "+ id;
                 Connection connection = statement.getConnection();
                 PreparedStatement ps1 = connection.prepareStatement(updateSql, PreparedStatement.RETURN_GENERATED_KEYS);
                 ps1.execute();
@@ -160,12 +160,17 @@ public class SignCommonUtil {
                     map.remove("UPDATE_TIME");
                     map.remove("CREATE_TIME");
                     String sign = MD5Utils.getInstance().getMD5(MapUtil.sort(map).toString());
-                    String updateSql = "update " + tableName + "  SET sign = " + "\'" + sign + "\'" + " where id = " + map.get("id").toString();
+                    String id;
+                    String updateSql = "";
+                    if (map.containsKey("id")) {
+                        id = map.get("id").toString();
+                        updateSql = "update " + tableName + "  SET sign = " + "\'" + sign + "\'" + " where id = " + id;
+                    }
 
-                    log.info("update set sign info1:"+tableName+"."+map.get("id").toString()+"/"+sign);
-                    log.info("update set sign info2:"+MapUtil.sort(map).toString());
-
-
+                    if (map.containsKey("BLIST_ID")) {
+                        id = map.get("BLIST_ID").toString();
+                        updateSql = "update " + tableName + "  SET sign = " + "\'" + sign + "\'" + " where BLIST_ID = " + id;
+                    }
                     sqlList.add(updateSql);
                 }
                 //rs.last();
@@ -184,6 +189,84 @@ public class SignCommonUtil {
 
     }
 
+
+    public void preCheckData(MappedStatement mappedStatement, Object parameterObject, Statement statement, BoundSql boundSql) throws SQLException, JSQLParserException {
+        boolean valid = true;
+        if (SqlCommandType.UPDATE.equals(mappedStatement.getSqlCommandType())) {
+            //解析sql,拿出where条件，构建查询sql获取更新范围的数据,进行验签
+            String sql = boundSql.getSql();
+            TypeHandlerRegistry typeHandlerRegistry = mappedStatement.getConfiguration().getTypeHandlerRegistry();
+            Configuration configuration = mappedStatement.getConfiguration();
+            List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+            if (parameterMappings != null) {
+                for (ParameterMapping parameterMapping : parameterMappings) {
+                    if (parameterMapping.getMode() != ParameterMode.OUT) {
+                        Object value;
+                        String propertyName = parameterMapping.getProperty();
+                        if (boundSql.hasAdditionalParameter(propertyName)) {
+                            value = boundSql.getAdditionalParameter(propertyName);
+                        } else if (parameterObject == null) {
+                            value = null;
+                        } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                            value = parameterObject;
+                        } else {
+                            value = configuration.newMetaObject(parameterObject).getValue(propertyName);
+                        }
+
+                        //类型处理
+                        if (value instanceof LocalDateTime) {
+                            value = DateUtil.format((LocalDateTime) value, DatePattern.NORM_DATETIME_PATTERN);
+                        } else if (value instanceof Date) {
+                            value = DateUtil.format((Date) value, DatePattern.NORM_DATETIME_PATTERN);
+                        } else if (value instanceof Boolean) {
+                            value = Boolean.FALSE.equals(value) ? "0" : "1";
+                        }
+                        //这里替换是为了防止value中存在?,导致sql替换出错
+                        String valueSet = String.valueOf(value).replaceAll("\\?", "！@#¥%");
+                        sql = StringUtils.replaceOnce(sql, "?", "'" + valueSet + "'");
+                    }
+                }
+            }
+            sql = sql.replaceAll("！@#¥%", "\\?");
+            Update update = (Update) CCJSqlParserUtil.parse(boundSql.getSql());
+            String tableName = update.getTable().getName();
+            String whereStr = " where" + sql.split("WHERE")[1];
+            String querySql = "select * from " + tableName + whereStr;
+
+            ResultSet rs = statement.getConnection().createStatement().executeQuery(querySql);
+            ResultSetMetaData md = rs.getMetaData();
+            Map<String, Object> map = new HashMap<>();
+            while (rs.next()) {
+
+                for (int i = 0; i < md.getColumnCount(); i++) {
+                    map.put(md.getColumnLabel(i + 1).toLowerCase(), rs.getObject(md.getColumnLabel(i + 1)));
+                }
+
+                if (map.get("sign") == null || Objects.equals(map.get("sign").toString(), "")) {
+                    return;
+                }
+
+                String oldSign = String.valueOf(map.get("sign"));
+                map.remove("sign");
+                map.remove("gmt_create");
+                map.remove("gmt_modified");
+                map.remove("gmt_update");
+                map.remove("create_time");
+                map.remove("update_time");
+                map.remove("UPDATE_TIME");
+                map.remove("CREATE_TIME");
+                String sign = MD5Utils.getInstance().getMD5(MapUtil.sort(map).toString());
+                if (!oldSign.equals(sign)) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (!valid) {
+                throw new TakinCloudException(TakinCloudExceptionEnum.DATA_SIGN_ERROR, "数据签名异常,请联系管理员!");
+            }
+        }
+    }
 
     public void validSign(MappedStatement mappedStatement, PreparedStatement ps) throws SQLException {
         List<ResultMap> resultMaps = mappedStatement.getResultMaps();
