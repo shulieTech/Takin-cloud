3e846e77c125db26f72552ac34b50b76c9c3aa97
张天赐 <zhangtianci@shulie.io>
2022-04-29 11:02:04 +0800

    任务文件.

---
+++ app/src/main/java/io/shulie/takin/cloud/app/entity/JobFileEntity.java
@@ -0,0 +1,44 @@
+package io.shulie.takin.cloud.app.entity;
+
+import java.util.Date;
+
+import lombok.Data;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**
+ * 数据库实体隐射 - 任务文件
+ *
+ * @author <a href="mailto:472546172@qq.com">张天赐</a>
+ */
+@Data
+@TableName("t_job_file")
+public class JobFileEntity {
+    /**
+     * 数据主键
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    /**
+     * 资源主键
+     */
+    private Long jobExampleId;
+    /**
+     * 统一资源标识符
+     */
+    private String uri;
+    /**
+     * 采样率
+     */
+    private Long startPoint;
+    /**
+     * 任务的运行模式
+     */
+    private Long endPoint;
+    /**
+     * 状态回调路径
+     */
+    private Date createTime;
+}
---
+++ app/src/main/java/io/shulie/takin/cloud/app/service/mapper/JobFileMapperService.java
@@ -0,0 +1,13 @@
+package io.shulie.takin.cloud.app.service.mapper;
+
+import io.shulie.takin.cloud.app.entity.JobFileEntity;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * Mapper - IService - 任务文件
+ *
+ * @author <a href="mailto:472546172@qq.com">张天赐</a>
+ */
+public interface JobFileMapperService extends IService<JobFileEntity> {
+}
