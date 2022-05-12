47785c10cf59f6569c07e1907429ea18dadea0c1
张天赐 <zhangtianci@shulie.io>
2022-05-12 21:10:08 +0800

    墓志铭

---
+++ app/src/main/java/io/shulie/takin/cloud/app/controller/GravestoneController.java
@@ -0,0 +1,101 @@
+package io.shulie.takin.cloud.app.controller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+import javax.annotation.Resource;
+
+import lombok.extern.slf4j.Slf4j;
+import cn.hutool.core.text.CharSequenceUtil;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+
+import io.shulie.takin.cloud.app.entity.JobEntity;
+import io.shulie.takin.cloud.app.service.JobService;
+import io.shulie.takin.cloud.model.response.ApiResult;
+import io.shulie.takin.cloud.app.entity.ResourceEntity;
+import io.shulie.takin.cloud.model.response.Gravestone;
+import io.shulie.takin.cloud.app.entity.JobExampleEntity;
+import io.shulie.takin.cloud.app.service.ResourceService;
+import io.shulie.takin.cloud.constant.enums.NotifyEventType;
+import io.shulie.takin.cloud.app.entity.ResourceExampleEntity;
+import io.shulie.takin.cloud.app.entity.JobExampleEventEntity;
+import io.shulie.takin.cloud.app.mapper.JobExampleEventMapper;
+import io.shulie.takin.cloud.app.entity.ResourceExampleEventEntity;
+import io.shulie.takin.cloud.app.mapper.ResourceExampleEventMapper;
+
+/**
+ * 墓志铭
+ *
+ * @author <a href="mailto:472546172@qq.com">张天赐</a>
+ */
+@Slf4j
+@RestController
+@RequestMapping("/gravestone")
+public class GravestoneController {
+    @Resource
+    JobService jobService;
+    @Resource
+    ResourceService resourceService;
+    @Resource
+    JobExampleEventMapper jobExampleEventMapper;
+    @Resource
+    ResourceExampleEventMapper resourceExampleEventMapper;
+
+    /**
+     * 读取墓志铭
+     *
+     * @return 内容
+     */
+    @RequestMapping("read")
+    public ApiResult<List<Gravestone>> read(@RequestParam Integer resourceId, @RequestParam(required = false) Integer jobId) {
+        List<Gravestone> result = new LinkedList<>();
+        ResourceEntity resource = resourceService.entity(resourceId);
+        if (resource != null) {
+            // 资源信息
+            result.add(new Gravestone(resource.getCreateTime().getTime(), "资源创建", String.valueOf(resource.getId())));
+            List<ResourceExampleEntity> resourceExample = resourceService.listExample(resource.getId());
+            resourceExample.forEach(t -> {
+                // 资源实例信息
+                result.add(new Gravestone(t.getCreateTime().getTime(), "资源实例创建", String.valueOf(t.getId())));
+                // 资源实例事件
+                List<ResourceExampleEventEntity> resourceExampleEvent = resourceExampleEventMapper.selectList(new LambdaQueryWrapper<>(ResourceExampleEventEntity.class)
+                    .eq(ResourceExampleEventEntity::getResourceExampleId, t.getId()));
+                resourceExampleEvent.forEach(c -> result.add(eventType(c.getTime(), c.getId(), c.getType(), c.getContext())));
+            });
+            if (jobId != null) {
+                // 任务信息
+                JobEntity job = jobService.jobEntity(jobId);
+                if (job != null) {
+                    // 任务信息
+                    result.add(new Gravestone(resource.getCreateTime().getTime(), "任务创建", String.valueOf(job.getId())));
+                    // 任务实例
+                    List<JobExampleEntity> jobExample = jobService.jobExampleEntityList(resource.getId());
+                    jobExample.forEach(t -> {
+                        // 任务实例信息
+                        result.add(new Gravestone(resource.getCreateTime().getTime(), "任务实例创建", String.valueOf(t.getId())));
+                        // 任务实例事件
+                        List<JobExampleEventEntity> jobExampleEvent = jobExampleEventMapper.selectList(new LambdaQueryWrapper<>(JobExampleEventEntity.class)
+                            .eq(JobExampleEventEntity::getJobExampleId, t.getId()));
+                        jobExampleEvent.forEach(c -> result.add(eventType(c.getTime(), c.getId(), c.getType(), c.getContext())));
+                    });
+                }
+            }
+        }
+        result.sort(Comparator.comparingLong(Gravestone::getTime));
+        // 返回结果
+        return ApiResult.success(result);
+    }
+
+    public Gravestone eventType(Date time, long id, Integer type, String content) {
+        NotifyEventType of = NotifyEventType.of(type);
+        String typeString = of == null ? CharSequenceUtil.format("未知({})", type) : of.getDescription();
+        String message = CharSequenceUtil.format("({}) - {}", id, content);
+        return new Gravestone(time.getTime(), typeString, message);
+    }
+}
--- constant/src/main/java/io/shulie/takin/cloud/constant/enums/CallbackType.java
+++ constant/src/main/java/io/shulie/takin/cloud/constant/enums/CallbackType.java
@@ -1,5 +1,9 @@
 package io.shulie.takin.cloud.constant.enums;
 
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
+
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 
@@ -30,4 +34,14 @@ public enum CallbackType {
     @JsonValue
     private final Integer code;
     private final String description;
+
+    private static final Map<Integer, CallbackType> EXAMPLE_MAP = new HashMap<>(8);
+
+    static {
+        Arrays.stream(values()).forEach(t -> EXAMPLE_MAP.put(t.getCode(), t));
+    }
+
+    public static CallbackType of(Integer code) {
+        return EXAMPLE_MAP.get(code);
+    }
 }
