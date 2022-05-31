7548cbe12043a31f09d437e8a583906e9e1ceb07
张天赐 <472546172@qq.com>
2022-05-31 15:39:19 +0800

    fix: 启动失败停止进程
    java.lang.Runtime#halt
    Forcibly terminates JMV.

--- app/src/main/java/io/shulie/takin/cloud/app/Application.java
+++ app/src/main/java/io/shulie/takin/cloud/app/Application.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,9 +18,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableScheduling
 @SpringBootApplication
 @MapperScan("io.shulie.takin.cloud.app.mapper")
-public class Application {
+public class Application implements DisposableBean {
     public static void main(String[] args) {
         System.setProperty("pagehelper.banner", Boolean.FALSE.toString());
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void destroy() {
+        log.warn("程序停止.");
+        Runtime.getRuntime().halt(0);
     }
 }
