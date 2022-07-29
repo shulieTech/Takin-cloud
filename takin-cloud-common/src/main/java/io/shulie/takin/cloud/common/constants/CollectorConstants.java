669f0fd8c1f3e93585c35f2df2a6d3b653b4f612
张天赐 <472546172@qq.com>
2022-07-29 14:24:26 +0800

    update:时间窗口的新方案

--- takin-cloud-common/src/main/java/io/shulie/takin/cloud/common/constants/CollectorConstants.java
+++ takin-cloud-common/src/main/java/io/shulie/takin/cloud/common/constants/CollectorConstants.java
@@ -15,6 +15,25 @@ public class CollectorConstants {
 
     /**
      * 窗口大小
+     * <p>
+     *     <span>* 动态方案</span>
+     *     <ul>
+     *         <li>n:时间戳</li>
+     *         <li>x:窗口大小</li>
+     *     </ul>
+     *     <hr/>
+     *     <strong>以7作为窗口大小为例:</strong>
+     *     <ul>
+     *         <li>[00,07)=0</li>
+     *         <li>[07,14)=7</li>
+     *         <li>[56,03)=56</li>
+     *         <li>[03,10)=063(第二分钟)</li>
+     *         <li>[06,13)=126(第三分钟)</li>
+     *         <li>[02,09)=182(第四分钟)</li>
+     *     </ul>
+     *     <hr/>
+     *     <strong>公式:</strong>{@code (~~(n/x)*x)+((n%x)>=0?0:-x)}
+     * </p>
      */
     public static int[] timeWindow = new int[] {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60};
 
@@ -29,7 +48,7 @@ public class CollectorConstants {
     /**
      * 10秒过期策略，超时丢弃Metrics 数据，单位：毫秒
      */
-    public static final long OVERDUE_TIME = 2000 * OVERDUE_SECOND;
+    public static final long OVERDUE_TIME = 2000L * OVERDUE_SECOND;
     /**
      * Metrics 统计时间间隔
      */
