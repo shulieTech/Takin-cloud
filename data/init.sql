128c0a81df0d9d79b8a94d16e09c57a54d971261
张天赐 <472546172@qq.com>
2022-08-22 10:35:56 +0800

    update:用索引代替外键

--- data/init.sql
+++ data/init.sql
@@ -22,8 +22,7 @@ create table if not exists t_callback_log
     request_time  timestamp  default CURRENT_TIMESTAMP not null comment '请求时间',
     response_data blob                                 null comment '响应数据',
     response_time datetime                             null comment '响应时间',
-    completed     tinyint(1) default 0                 not null comment '是否完成',
-    constraint t_callback_log_t_callback_id_fk foreign key (callback_id) references t_callback (id)
+    completed     tinyint(1) default 0                 not null comment '是否完成'
 ) comment '回调日志';
 
 create table if not exists t_file
@@ -39,7 +38,7 @@ create table if not exists t_watchman
     ref        varchar(512)            not null comment '关键词',
     sign       varchar(255)            not null comment '关键词签名',
     public_key varchar(512) default '' not null comment '数据加密的公钥',
-    constraint t_watchman_ref_sign_uindex unique (sign)
+    constraint t_watchman_sign_uindex unique (sign)
 ) comment '调度器';
 
 create table if not exists t_command
@@ -50,8 +49,7 @@ create table if not exists t_command
     type        int                                 not null comment '命令类型',
     content     json                                not null comment '命令内容',
     ack_time    datetime                            null comment '命令确认时间',
-    ack_content json                                null comment '命令确认内容',
-    constraint t_command_t_watchman_id_fk foreign key (watchman_id) references t_watchman (id)
+    ack_content json                                null comment '命令确认内容'
 ) comment '命令表';
 
 create table if not exists t_file_example
@@ -66,9 +64,7 @@ create table if not exists t_file_example
     complete_size bigint       default 0  not null comment '完成的大小',
     total_size    bigint       default -1 not null comment '总大小',
     completed     tinyint(1)              null comment '已完成',
-    message       varchar(255)            null comment '消息',
-    constraint t_file_example_t_watchman_id_fk foreign key (watchman_id) references t_watchman (id),
-    constraint t_file_manage_list_t_file_manage_id_fk foreign key (file_id) references t_file (id)
+    message       varchar(255)            null comment '消息'
 ) comment '文件实例';
 
 create table if not exists t_script
@@ -81,8 +77,7 @@ create table if not exists t_script
     start_time   datetime default CURRENT_TIMESTAMP not null comment '开始时间',
     completed    tinyint(1)                         null comment '是否完成',
     message      varchar(255)                       null comment '执行结果',
-    end_time     datetime                           null comment '结束时间',
-    constraint t_script_t_watchman_id_fk foreign key (watchman_id) references t_watchman (id)
+    end_time     datetime                           null comment '结束时间'
 ) comment '脚本校验任务';
 
 create table if not exists t_resource
@@ -128,8 +123,7 @@ create table if not exists t_calibration_log
     id             bigint auto_increment primary key,
     calibration_id bigint       not null,
     content        varchar(255) null,
-    completed      tinyint(1)   null,
-    constraint t_calibration_log_t_calibration_id_fk foreign key (calibration_id) references t_calibration (id)
+    completed      tinyint(1)   null
 );
 
 create table if not exists t_metrics_config
@@ -137,8 +131,7 @@ create table if not exists t_metrics_config
     id          bigint auto_increment comment '主键' primary key,
     pressure_id bigint       not null comment '压测任务主键',
     ref         varchar(255) not null comment '关键字',
-    context     json         null comment '目标值',
-    constraint t_metrics_t_pressure_id_fk foreign key (pressure_id) references t_pressure (id)
+    context     json         null comment '目标值'
 ) comment '指标信息';
 
 create table if not exists t_resource_example
@@ -151,8 +144,7 @@ create table if not exists t_resource_example
     limit_cpu    varchar(255)                        not null comment '限定的CPU',
     limit_memory varchar(255)                        not null comment '限定的内存',
     create_time  timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    image        varchar(1024)                       null comment '资源镜像信息',
-    constraint t_resource_example_t_resource_id_fk foreign key (resource_id) references t_resource (id)
+    image        varchar(1024)                       null comment '资源镜像信息'
 ) comment '资源实例表';
 
 create table if not exists t_pressure_example
@@ -161,9 +153,7 @@ create table if not exists t_pressure_example
     resource_example_id bigint        not null comment '资源实例主键',
     number              int default 0 not null comment '序列号',
     duration            bigint        not null comment '持续时长(毫秒)',
-    pressure_id         bigint        not null comment '任务主键',
-    constraint t_pressure_example_t_pressure_id_fk foreign key (pressure_id) references t_pressure (id),
-    constraint t_pressure_example_t_resource_example_id_fk foreign key (resource_example_id) references t_resource_example (id)
+    pressure_id         bigint        not null comment '任务主键'
 ) comment '任务实例';
 
 create table if not exists t_pressure_example_event
@@ -172,8 +162,7 @@ create table if not exists t_pressure_example_event
     pressure_example_id bigint                              not null comment '任务实例主键',
     type                int                                 not null comment '事件类型',
     context             json                                not null comment '事件内容',
-    time                timestamp default CURRENT_TIMESTAMP not null comment '时间',
-    constraint t_pressure_example_event_t_pressure_example_id_fk foreign key (pressure_example_id) references t_pressure_example (id)
+    time                timestamp default CURRENT_TIMESTAMP not null comment '时间'
 ) comment '任务实例事件';
 
 create table if not exists t_pressure_file
@@ -185,8 +174,7 @@ create table if not exists t_pressure_file
     type                int                                 not null comment '文件类型',
     start_point         bigint                              not null comment '读取文件的起始点位',
     end_point           mediumtext                          not null comment '读取文件的结束点位',
-    create_time         timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    constraint t_pressure_file_t_pressure_example_id_fk foreign key (pressure_example_id) references t_pressure_example (id)
+    create_time         timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
 ) comment '任务所需文件';
 
 create table if not exists t_resource_example_event
@@ -195,8 +183,7 @@ create table if not exists t_resource_example_event
     resource_example_id bigint                              not null comment '资源实例主键',
     type                int                                 not null comment '事件类型',
     context             json                                not null comment '事件内容',
-    time                timestamp default CURRENT_TIMESTAMP not null comment '时间',
-    constraint t_resource_example_event_t_resource_example_id_fk foreign key (resource_example_id) references t_resource_example (id)
+    time                timestamp default CURRENT_TIMESTAMP not null comment '时间'
 ) comment '资源实例事件';
 
 create table if not exists t_sla
@@ -207,8 +194,7 @@ create table if not exists t_sla
     attach         varchar(255) null comment '附加数据',
     formula_target int          null comment '算式目标(RT、TPS、SA、成功率)',
     formula_symbol int          null comment '算式符号(>=、>、=、<=、<)',
-    formula_number double       not null comment '算式数值(用户输入)',
-    constraint t_sla_t_pressure_id_fk foreign key (pressure_id) references t_pressure (id)
+    formula_number double       not null comment '算式数值(用户输入)'
 ) comment 'Service Level Agreement(服务等级协议)';
 
 create table if not exists t_sla_event
@@ -222,9 +208,7 @@ create table if not exists t_sla_event
     formula_target      int          null comment '算式目标(RT、TPS、SA、成功率)',
     formula_symbol      int          null comment '算式符号(>=、>、=、<=、<)',
     formula_number      double       not null comment '算式数值(用户输入)',
-    number              double       not null comment '比较的值(实际变化的值)',
-    constraint t_sla_event_t_pressure_example_id_fk foreign key (pressure_example_id) references t_pressure_example (id),
-    constraint t_sla_event_t_sla_id_fk foreign key (sla_id) references t_sla (id)
+    number              double       not null comment '比较的值(实际变化的值)'
 ) comment 'sla触发记录';
 
 create table if not exists t_thread_config
@@ -233,8 +217,7 @@ create table if not exists t_thread_config
     pressure_id bigint       not null comment '任务主键',
     ref         varchar(255) not null comment '关键字',
     mode        int          not null comment '模式',
-    context     json         not null comment '线程配置',
-    constraint t_thread_config_t_pressure_id_fk foreign key (pressure_id) references t_pressure (id)
+    context     json         not null comment '线程配置'
 ) comment '线程配置';
 
 create table if not exists t_thread_config_example
@@ -246,8 +229,7 @@ create table if not exists t_thread_config_example
     ref                 varchar(255)  not null comment '关键字',
     type                int           not null comment '类型',
     context             json          not null comment '线程配置',
-    update_time         timestamp     null on update CURRENT_TIMESTAMP comment '修改时间',
-    constraint t_thread_config_example_t_pressure_example_id_fk foreign key (pressure_example_id) references t_pressure_example (id)
+    update_time         timestamp     null on update CURRENT_TIMESTAMP comment '修改时间'
 ) comment '线程配置实例';
 
 create table if not exists t_watchman_event
@@ -256,11 +238,32 @@ create table if not exists t_watchman_event
     watchman_id bigint                              not null comment '调度器主键',
     time        timestamp default CURRENT_TIMESTAMP not null comment '时间',
     type        int                                 not null comment '状态',
-    context     json                                not null comment '事件内容',
-    constraint t_watchman_event_t_watchman_id_fk foreign key (watchman_id) references t_watchman (id)
+    context     json                                not null comment '事件内容'
 ) comment '调度器事件';
 
+-- 查询索引
 create index t_watchman_event_time_index on t_watchman_event (time);
-
 create index t_watchman_event_type_index on t_watchman_event (type);
-
+-- 外键索引 - watchman_id
+create index t_script_watchman_id_index on t_script (watchman_id);
+create index t_command_watchman_id_index on t_command (watchman_id);
+create index t_file_example_watchman_id_index on t_file_example (watchman_id);
+create index t_watchman_event_watchman_id_index on t_watchman_event (watchman_id);
+-- 外键索引 - pressure
+create index t_sla_pressure_id_index on t_sla (pressure_id);
+create index t_sla_event_sla_id_index on t_sla_event (sla_id);
+create index t_thread_config_pressure_id_index on t_thread_config (pressure_id);
+create index t_metrics_config_pressure_id_index on t_metrics_config (pressure_id);
+create index t_pressure_example_pressure_id_index on t_pressure_example (pressure_id);
+create index t_sla_event_pressure_example_id_index on t_sla_event (pressure_example_id);
+create index t_pressure_file_pressure_example_id_index on t_pressure_file (pressure_example_id);
+create index t_thread_config_example_pressure_example_id_index on t_thread_config_example (pressure_example_id);
+create index t_pressure_example_event_pressure_example_id_index on t_pressure_example_event (pressure_example_id);
+-- 外键索引 - resource
+create index t_resource_example_resource_id_index on t_resource_example (resource_id);
+create index t_pressure_example_resource_example_id_index on t_pressure_example (resource_example_id);
+create index t_resource_example_event_resource_example_id_index on t_resource_example_event (resource_example_id);
+-- 外键索引 - 其它
+create index t_file_example_file_id_index on t_file_example (file_id);
+create index t_callback_log_t_callback_id_index on t_callback_log (callback_id);
+create index t_calibration_log_calibration_id_index on t_calibration_log (calibration_id);
