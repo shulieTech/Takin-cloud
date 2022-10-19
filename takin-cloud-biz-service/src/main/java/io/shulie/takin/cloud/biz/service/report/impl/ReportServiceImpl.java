39bc8397954f3cd744474ab4ba35ad50c0a3d56b
张天赐 <472546172@qq.com>
2022-10-19 14:16:47 +0800

    fix: 日志压缩禁用sudo
    权限问题

--- takin-cloud-biz-service/src/main/java/io/shulie/takin/cloud/biz/service/report/impl/ReportServiceImpl.java
+++ takin-cloud-biz-service/src/main/java/io/shulie/takin/cloud/biz/service/report/impl/ReportServiceImpl.java
@@ -1655,7 +1655,7 @@ public class ReportServiceImpl implements ReportService {
             return jtlPath + "/" + "Jmeter.zip";
         } else if (needZip) {
             // 开始压缩
-            String command = StrUtil.indexedFormat("sudo zip -r -j {0}/Jmeter.zip {0} {1}", jtlPath, logPath);
+            String command = StrUtil.indexedFormat("zip -r -j {0}/Jmeter.zip {0} {1}", jtlPath, logPath);
             log.info("压测日志打包成文件:{}", command);
             Boolean result = LinuxHelper.executeLinuxCmd(command);
             if (result) {
