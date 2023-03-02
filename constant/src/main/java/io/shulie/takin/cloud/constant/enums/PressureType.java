fef2ee02ec7245051509c684e3ec1faed8b0adb6
zhaoyong <zhaoyong@shulie.io>
2023-03-02 18:37:10 +0800

    修改PressureType构造函数

--- constant/src/main/java/io/shulie/takin/cloud/constant/enums/PressureType.java
+++ constant/src/main/java/io/shulie/takin/cloud/constant/enums/PressureType.java
@@ -1,9 +1,14 @@
 package io.shulie.takin.cloud.constant.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 施压类型
@@ -30,11 +35,23 @@ public enum PressureType {
     TRY(5, "试跑模式"),
     // 格式化用
     ;
+
     @Getter
     @JsonValue
     private final Integer code;
     @Getter
     private final String description;
+
+    private static final Map<Integer, PressureType> CODE_MAPPING = new HashMap<>(8);
+
+    static {
+        Arrays.stream(values()).forEach(t -> CODE_MAPPING.put(t.getCode(), t));
+    }
+
+    @JsonCreator
+    public static PressureType of(Integer code) {
+        return CODE_MAPPING.get(code);
+    }
 
     @Override
     public String toString() {return code + ":" + description;}
