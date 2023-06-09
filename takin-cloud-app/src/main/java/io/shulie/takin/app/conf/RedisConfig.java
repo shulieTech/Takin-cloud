a47a3cafd742adfe64da3f2b40b548b74b2daecd
jiangjibo <jiangjibo@shulie.io>
2023-06-09 18:02:43 +0800

    redis序列化反序列化调整

--- takin-cloud-app/src/main/java/io/shulie/takin/app/conf/RedisConfig.java
+++ takin-cloud-app/src/main/java/io/shulie/takin/app/conf/RedisConfig.java
@@ -5,8 +5,15 @@ import javax.annotation.Resource;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +33,7 @@ public class RedisConfig implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         // 为了解决不同工程，序列化问题
-        FastJsonRedisSerializer<Object> valueSerializer = new FastJsonRedisSerializer<>(Object.class);
+        Jackson2JsonRedisSerializer<Object> valueSerializer = getJackson2JsonRedisSerializer();
         // 建议使用这种方式，小范围指定白名单
         ParserConfig.getGlobalInstance().addAccept("io.shulie.");
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -35,6 +42,17 @@ public class RedisConfig implements InitializingBean {
         redisTemplate.setHashValueSerializer(valueSerializer);
         redisTemplate.setDefaultSerializer(valueSerializer);
         redisTemplate.afterPropertiesSet();
+    }
+
+    private Jackson2JsonRedisSerializer<Object> getJackson2JsonRedisSerializer() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(
+                Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        return jackson2JsonRedisSerializer;
     }
 
 }
